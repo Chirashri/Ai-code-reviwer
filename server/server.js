f0c3f746568908b97d7cd3e0030026ddaf70f5e2
unknown <chirashri261004@gmail.com>
2026-03-15 16:20:08 +0530

    fix render port binding

--- server/server.js
+++ server/server.js
@@ -238,5 +238,5 @@ app.post("/explain", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
